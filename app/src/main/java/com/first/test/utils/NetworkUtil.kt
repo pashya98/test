5df298123f91e811caa26c9db68c9a70967ddbe7
mohini <mohini.raghuwanshi@neosofttech.com>
2019-04-12 11:58:17 +0530

    profile dimesion updated

--- app/src/main/java/com/first/test/utils/NetworkUtil.kt
+++ app/src/main/java/com/first/test/utils/NetworkUtil.kt
@@ -5,7 +5,6 @@ import android.net.ConnectivityManager
 
 class NetworkUtil {
     companion object {
-
         fun isNetworkConnected(context: Context): Boolean {
             val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
             val activeNetwork = cm.activeNetworkInfo
